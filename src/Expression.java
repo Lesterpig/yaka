98d089fa33ee21dd7a5751445f0b6b70abc06c72
ShadowsWalker <axelcaro@hotmail.fr>
2015-03-03 15:07:50 +0100

    Creation de la classe operateur, de certaines de ses methode. Mise a jour de la classe Expression et ajout de quelques methodes.

--- src/Expression.java
+++ src/Expression.java
@@ -1,15 +1,44 @@
 import java.util.Stack;
+import java.util.EmptyStackException;
 
 public class Expression {
     
     private static Stack<TypeList> types;
-    private static Stack<Character> operateurs;
+    private static Stack<Operateur> operateurs;
 
     private static String errorLog;
 
     public static void initPiles() {
         types = new Stack<TypeList>();
-        operateurs = new Stack<Character>();
+        operateurs = new Stack<Operateur>();
     }
 
+	public void ajoutOperateur(Operateur op) {
+		operateurs.add(op);
+	}
+	
+	public Operateur retraitOperateur() throws EmptyStackException {
+		return operateurs.pop();
+	}
+	
+	public void ajoutType(TypeList t) {
+		types.add(t);
+	}
+	
+	public TypeList retraitType() throws EmptyStackException {
+		return types.pop();
+	}
+	
+	public void traiterOperation() {
+		try {
+			Operateur op = retraitOperateur();
+			//dans la pratique, du fait de l'utilisation d'un objet 
+			//Expression de maniere statique, l'appel sur cet objet est 
+			//justifie car les operateurs sont uniquement lies a cette instance.
+			op.valider(this);
+		} catch (EmptyStackException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
