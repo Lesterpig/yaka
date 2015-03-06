d72d9db9d9614fb01293898d9cc5b75e3915e593
Axel Caro <axel.caro@insa-rennes.fr>
2015-03-06 09:08:20 +0100

    Quelques getters et setters...

--- src/compiler/Declaration.java
+++ src/compiler/Declaration.java
@@ -5,9 +5,25 @@ public class Declaration {
 	private TypeList type;
 	private String ident;
 
-
 	public Declaration() {
 		type = TypeList.ERREUR;
 		ident = "";
 	}
+	
+	public TypeList getType () {
+		return type;
+	}
+	
+	public void setType(TypeList t) {
+		type = t;
+	}
+	
+	public String getIdent() {
+		return ident;
+	}
+	
+	public void setIdent(String i) {
+		ident = i;
+	}
+	
 }
