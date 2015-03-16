6e8d787ff3b1ee47e1c335b55ac7eee6682a0701
Quentin Dufour <quentin@dufour.tk>
2015-03-16 11:43:16 +0100

    Ajout du script powershell

--- asm.bat
+++ asm.bat
@@ -1,5 +1,8 @@
 @echo off
 REM From : https://github.com/nhurman/yaka-compiler/blob/master/bin/asm.bat
-tasm /l /zi lib\biblio.asm
-tasm /l /zi %1.asm
-tlink /v %1 %1 biblio.obj
+
+X:\tasm /l /zi lib\biblio.asm
+
+X:\tasm /l /zi %1.asm
+
+X:\tlink /v %1 %1 biblio.obj
---
+++ yaka.ps1
@@ -0,0 +1,2 @@
+
+cat .\example\exp.yaka| & 'C:\Program Files (x86)\Java\jre8\bin\java.exe' -jar .\Yaka.jar | out-file -encoding ascii -filepath yaka.asm
