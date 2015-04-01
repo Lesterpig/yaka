615f9a8bdef1dfac42942a1e3543b41b18e9053b
Quentin Dufour <quentin@dufour.tk>
2015-04-01 11:46:05 +0200

    Ajout du fichier de test

---
+++ test/compiler/TabIdentTest.java
@@ -0,0 +1,36 @@
+package compiler;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+import generated.Yaka;
+
+public class TabIdentTest {
+
+    private TabIdent t;
+
+    @Before
+    public void init() {
+        this.t = new TabIdent();
+    }
+
+    @Test
+    public void searchIdentTest() {
+        t.addVariable("john", TypeList.ENTIER);
+        Ident i = t.searchIdent("john");
+        assertEquals(i.getType(), TypeList.ENTIER);
+        assertEquals(Yaka.errorLog, "");
+
+        t.addVariable("fail", TypeList.FONCTION);
+        assertEquals(t.searchIdent("fail"), null);
+        assertNotEquals(Yaka.errorLog, "");
+    }
+
+    @Test
+    public void searchFnTest() {
+        IdFn i = new IdFn(TypeList.ENTIER);
+        t.addFn("ripper", i);
+        assertEquals(t.searchFn("ripper"), i);
+    }
+
+}
